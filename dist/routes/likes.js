e0f0f07ee70b495eadf1882fd2b10dbfe8da2052
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-07-02 14:43:03 -0700

    altered endpoint to reflect difference between post likes and comment likes to be implemented

--- dist/routes/likes.js
+++ dist/routes/likes.js
@@ -6,9 +6,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 const likes_controller_1 = require("../controllers/likes_controller");
-router.get('/all', (req, res) => {
-    res.json("Likes Here");
-});
-router.post('/:UserId/:PostId', likes_controller_1.create_post_like);
-router.delete('/:UserId/:PostId', likes_controller_1.remove_post_like);
+router.post('/post/:PostId/:UserId', likes_controller_1.create_post_like);
+router.delete('/post/:PostId/:UserId', likes_controller_1.remove_post_like);
 exports.default = router;
--- routes/likes.ts
+++ routes/likes.ts
@@ -2,12 +2,8 @@ import express from "express";
 const router = express.Router();
 import { create_post_like, remove_post_like } from '../controllers/likes_controller';
 
-router.get('/all', (req, res) => {
-    res.json("Likes Here")
-});
+router.post('/post/:PostId/:UserId', create_post_like);
 
-router.post('/:UserId/:PostId', create_post_like);
-
-router.delete('/:UserId/:PostId', remove_post_like)
+router.delete('/post/:PostId/:UserId', remove_post_like)
 
 export default router;
