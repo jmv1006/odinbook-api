d3a315c39f0bec859be1b91ef985a7e2a3c45c86
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-08-15 09:15:02 -0700

    changes to way dotenv is loaded

--- app.ts
+++ app.ts
@@ -1,11 +1,9 @@
+import 'dotenv/config';
 import  {Request, Response} from 'express';
 import express from "express";
 import passport from 'passport';
 import cookieParser from 'cookie-parser';
 import { connectToRedis } from './config/redis/redis.config';
-import * as dotenv from 'dotenv';
-
-dotenv.config({path: `.env.development`})
 
 import cors from 'cors';
 
