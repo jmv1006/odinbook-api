c06dd8e33c8d6a6b7179b12505446a4bfe4f193c
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2022-08-15 09:25:24 -0700

    re-added authentication to routes

--- app.ts
+++ app.ts
@@ -54,9 +54,9 @@ app.use('/auth', authRoute);
 app.use('/posts', passport.authenticate('jwt', {session: false}), postsRoute);
 app.use('/likes', passport.authenticate('jwt', {session: false}), likesRoute);
 app.use('/comments', passport.authenticate('jwt', {session: false}), commentsRoute);
-app.use('/users', userRoute);
+app.use('/users', passport.authenticate('jwt', {session: false}), userRoute);
 app.use('/friend-requests', passport.authenticate('jwt', {session: false}), friendRequestsRoute);
-app.use('/friendships', friendshipsRoute);
+app.use('/friendships', passport.authenticate('jwt', {session: false}), friendshipsRoute);
 
 
 export default app;
